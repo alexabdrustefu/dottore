822f7e47bb54458395ba0095d9aa6c01854ac6c5
alexabdrustefu <alessandro.sstefu06@gmail.com>
2023-05-26 15:23:08 +0200

    primo commit

---
+++ src/main/java/it/prova/dottore/DottoreApplication.java
@@ -0,0 +1,13 @@
+package it.prova.dottore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DottoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DottoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/prova/dottore/DottoreApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.dottore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DottoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
